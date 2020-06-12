32b968e139becc767b826eb483f91fd2ca01223a
Keresifon <keresifon@gmail.com>
2020-06-12 23:20:25 +0100

    Update header.js

--- src/component/header.js
+++ src/component/header.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap"
+import { Navbar, Nav, Form, FormControl, Button, NavDropdown } from "react-bootstrap"
 
 //import  "../styles/main.scss"
 
@@ -19,18 +19,23 @@ const Header = () => {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse>
           <Nav className="ml-auto ">
-            <Form inline>
+            {/* <Form inline>
               <FormControl
                 type="text"
                 placeholder="Search"
                 className="mr-sm-2"
               />
               <Button variant="outline-primary">Search</Button>
-            </Form>
+            </Form> */}
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="#features">The Blog</Nav.Link>
-            <Nav.Link href="#pricing">All Categories</Nav.Link>
-            <Nav.Link href="/category/Technology">Technology</Nav.Link>
+            <NavDropdown title="All Categories" id="basic-nav-dropdown">
+        <NavDropdown.Item href="/category/Technology">Technology</NavDropdown.Item>
+        <NavDropdown.Item href="/category/Opinion">Opinion</NavDropdown.Item>
+        <NavDropdown.Item href="/category/History">History</NavDropdown.Item>
+        {/* <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item> */}
+      </NavDropdown>
             <Nav.Link href="#pricing">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
